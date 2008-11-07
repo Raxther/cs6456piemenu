ebaa5adb98c0ba4ec8e8a3b12450ee36acfbccf7
luiscruz0530 <luiscruz0530@863d9168-aced-11dd-9b86-41075523de00>
2008-11-07 18:59:06 +0000

    skeleton code

---
+++ PieMenu/src/PieMenu.java
@@ -0,0 +1,55 @@
+import javax.swing.JComponent;
+import javax.swing.UIManager;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ * 
+ */
+
+/**
+ * @author Ivan & Luis
+ *
+ */
+public class PieMenu extends JComponent implements ChangeListener {
+	
+	private PieMenuModel pieMenuModel;
+	
+	public PieMenu() {
+		pieMenuModel = new PieMenuModel();
+		setModel();
+		updateUI();
+	}
+
+	public String getUIClassID() {
+		return PieMenuUI.UI_CLASS_ID;
+	}
+	
+	public void setModel() {
+		if(pieMenuModel != null) {
+			pieMenuModel.removeChangeListener(this);
+		}
+		pieMenuModel.addChangeListener(this);
+	}
+	
+	public PieMenuModel getModel() {
+		return pieMenuModel;
+	}
+	
+	public void setUI(PieMenuUI ui) {
+		super.setUI(ui);
+	}
+
+	public void updateUI() {
+		setUI((PieMenuUI) UIManager.getUI(this));
+		invalidate();
+	}
+	
+	
+	@Override
+	public void stateChanged(ChangeEvent arg0) {
+		repaint();
+		
+	}
+
+}
