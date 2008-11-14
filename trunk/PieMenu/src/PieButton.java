1d4ee454a068e894eda60e819817c7cbbfaa0b29
rogue3092 <rogue3092@863d9168-aced-11dd-9b86-41075523de00>
2008-11-14 05:50:08 +0000

    Hierarchical button stuff

--- trunk/PieMenu/src/PieButton.java
+++ trunk/PieMenu/src/PieButton.java
@@ -19,6 +19,7 @@ public class PieButton extends JComponent implements ChangeListener {
 
 	private PieButtonModel pieButtonModel;
 	private JButton mainButton;
+        public JButton[] hierarchButtons;
 	private int degree;
 
 	public PieButton() {
@@ -66,10 +67,33 @@ public class PieButton extends JComponent implements ChangeListener {
 		mainButton.setBounds(0, 0, 44, 44);
 	}
 	
+        public void addHierarchButtons(int numButtons) {
+                
+                hierarchButtons = new JButton[numButtons];
+                for(int i=0; i< hierarchButtons.length; i++)
+                {
+                    JButton hierarchButton = new JButton();
+                    hierarchButton.setIcon(new ImageIcon(getClass().getResource(
+                                    "resources/dark_blue_button.png")));
+                    hierarchButton.setRolloverIcon(new ImageIcon(getClass().getResource(
+                                    "resources/yellow_button.png")));
+                    hierarchButton.setFocusPainted(false);
+                    hierarchButton.setContentAreaFilled(false);
+                    hierarchButton.setBorderPainted(false);
+
+                    this.add(hierarchButton);
+                    hierarchButton.setBounds(0, 0, 44, 44);
+                    hierarchButtons[i] = hierarchButton;
+                }
+        }
+        
 	public void setDegree(int d) {
 		degree = d;
 	}
 
+        public int getDegree() {
+		return(degree);
+        }
 	@Override
 	public void stateChanged(ChangeEvent arg0) {
 		repaint();
