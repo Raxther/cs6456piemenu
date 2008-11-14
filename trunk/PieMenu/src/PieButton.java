aad3837b92da2c4d9f26b2ec18b4b3efe24df8ba
luiscruz0530 <luiscruz0530@863d9168-aced-11dd-9b86-41075523de00>
2008-11-14 07:16:11 +0000

    Refactoring of addHierarchy() to PieButton class

--- trunk/PieMenu/src/PieButton.java
+++ trunk/PieMenu/src/PieButton.java
@@ -19,13 +19,18 @@ public class PieButton extends JComponent implements ChangeListener {
 
 	private PieButtonModel pieButtonModel;
 	private JButton mainButton;
-        public JButton[] hierarchButtons;
+	public JButton[] hierarchButtons;
 	private int degree;
+	private int leafNodes;
 
-	public PieButton() {
+	public PieButton(int nodes) {
 		pieButtonModel = new PieButtonModel();
 		setModel();
 		buildButton();
+		leafNodes = nodes;
+		if (leafNodes > 0) {
+			addHierarchButtons(leafNodes);
+		}
 		updateUI();
 	}
 
@@ -66,38 +71,66 @@ public class PieButton extends JComponent implements ChangeListener {
 		this.add(mainButton);
 		mainButton.setBounds(0, 0, 44, 44);
 	}
-	
-        public void addHierarchButtons(int numButtons) {
-                
-                hierarchButtons = new JButton[numButtons];
-                for(int i=0; i< hierarchButtons.length; i++)
-                {
-                    JButton hierarchButton = new JButton();
-                    hierarchButton.setIcon(new ImageIcon(getClass().getResource(
-                                    "resources/dark_blue_button.png")));
-                    hierarchButton.setRolloverIcon(new ImageIcon(getClass().getResource(
-                                    "resources/yellow_button.png")));
-                    hierarchButton.setFocusPainted(false);
-                    hierarchButton.setContentAreaFilled(false);
-                    hierarchButton.setBorderPainted(false);
 
-                    this.add(hierarchButton);
-                    hierarchButton.setBounds(0, 0, 44, 44);
-                    hierarchButtons[i] = hierarchButton;
-                }
-        }
-        
+	private void addHierarchButtons(int numButtons) {
+		hierarchButtons = new JButton[numButtons];
+		for (int i = 0; i < hierarchButtons.length; i++) {
+			JButton hierarchButton = new JButton();
+			hierarchButton.setIcon(new ImageIcon(getClass().getResource(
+					"resources/dark_blue_button.png")));
+			hierarchButton.setRolloverIcon(new ImageIcon(getClass()
+					.getResource("resources/yellow_button.png")));
+			hierarchButton.setFocusPainted(false);
+			hierarchButton.setContentAreaFilled(false);
+			hierarchButton.setBorderPainted(false);
+
+			this.add(hierarchButton);
+			hierarchButton.setBounds(0, 0, 44, 44);
+			hierarchButtons[i] = hierarchButton;
+		}
+	}
+
+	public void updateHierarchy() {
+		if (leafNodes > 0) {
+			double currentHierarchAngle = 0;
+			// Position hierarchical buttons
+			for (int y = 0; y < hierarchButtons.length; y++) {
+
+				// Spacing - currently hard coded to 55 degrees
+				int hierarchSpacing = 55;
+				// Get coordinates of parent button
+				int parentButtonX = this.getX();
+				int parentButtonY = this.getY();
+				// Get current angles (in radians)
+				double currentHierarchXAngle = Math.cos(Math.toRadians(this
+						.getDegree()
+						+ currentHierarchAngle));
+				double currentHierarchYAngle = Math.sin(Math.toRadians(this
+						.getDegree()
+						+ currentHierarchAngle));
+				// Get current offset coordinates (50 pixel maximum offset - for
+				// two buttons, otherwise need more)
+				double currentHierarchXCoordinate = 50 * currentHierarchXAngle;
+				double currentHierarchYCoordinate = 50 * currentHierarchYAngle;
+				hierarchButtons[y].setBounds(parentButtonX
+						+ (int) currentHierarchXCoordinate, parentButtonY
+						- (int) currentHierarchYCoordinate, 44, 44);
+				currentHierarchAngle += hierarchSpacing;
+			}
+		}
+	}
+
 	public void setDegree(int d) {
 		degree = d;
 	}
 
-        public int getDegree() {
-		return(degree);
-        }
+	public int getDegree() {
+		return (degree);
+	}
+
 	@Override
 	public void stateChanged(ChangeEvent arg0) {
 		repaint();
-
 	}
 
 }
